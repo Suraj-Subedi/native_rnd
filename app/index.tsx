e1c646f9e6e56f3bc85e3f5024a01e9541e5eb47
Suraj Subedi <soorajsubedi1227@gmail.com>
2024-07-02 22:38:17 +0545

    basic route config and tailwind config

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import {Link} from "expo-router";
+import {StatusBar} from "expo-status-bar";
+import {StyleSheet, Text, View} from "react-native";
+
+const App = () => {
+  return (
+    <View className="flex flex-1 items-center justify-center">
+      <Text className="text-3xl text-rose-500">{"Futura"}</Text>
+      <StatusBar style="auto" />
+      <Link href="/profile" style={{color: "blue"}}>
+        Go to profile
+      </Link>
+    </View>
+  );
+};
+
+export default App;
